1ba6d506682e9a34642cf999d63a06d1608fd1b4
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-05-28 21:01:10 +0200

    :lipstick: update header color ui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,17 +3,21 @@
 import Image from "next/image";
 import { LogIn, LogOut } from "lucide-react";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Header() {
   const [isConnected, setIsConnected] = useState(true);
 
   return (
     <header className="p-8 m-auto w-full">
-      <nav className="rounded-lg bg-indigo-950 backdrop-blur-xl flex items-center justify-between px-8 py-4 relative">
-        <div className="flex items-center gap-5">
-          <Image src="/img/logo.png" alt="logo" width={25} height={25} />
-          <h3 className="hidden md:block text-xl font-semibold">Learn404</h3>
-        </div>
+      <nav className="rounded-lg bg-black bg-opacity-20 backdrop-blur-xl flex items-center justify-between px-8 py-4 relative">
+        <Link href="/">
+          <div className="flex items-center gap-5">
+            <Image src="/img/logo.png" alt="logo" width={25} height={25} />
+            <h3 className="hidden md:block text-xl font-semibold">Learn404</h3>
+          </div>
+        </Link>
+
         {/* <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
           <ul className="flex items-center gap-5 text-gray-600">
             <li>
@@ -27,11 +31,11 @@ export default function Header() {
         <div className="flex items-center gap-2">
           {isConnected ? (
             <>
-              <button className="rounded-lg border backdrop-blur border-gray-600 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-600 transition-all dureation-300 ease-in-out">
+              <button className="rounded-lg border backdrop-blur border-gray-400 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-400 hover:bg-opacity-50 transition-all dureation-300 ease-in-out">
                 <span className="text-sm md:text-base font-medium">Lesson</span>
               </button>
               <button
-                className="rounded-lg border backdrop-blur border-gray-600 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-600 transition-all dureation-300 ease-in-out"
+                className="rounded-lg border backdrop-blur border-gray-400 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-400 hover:bg-opacity-50 transition-all dureation-300 ease-in-out"
                 onClick={() => setIsConnected(false)}
               >
                 <LogOut size={20} />
@@ -42,7 +46,7 @@ export default function Header() {
             </>
           ) : (
             <button
-              className="rounded-lg border backdrop-blur border-gray-600 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-600 transition-all dureation-300 ease-in-out"
+              className="rounded-lg border backdrop-blur border-gray-400 text-white px-5 py-1 flex items-center gap-2 hover:bg-gray-400 hover:bg-opacity-50 transition-all dureation-300 ease-in-out"
               onClick={() => setIsConnected(true)}
             >
               <LogIn size={20} />
