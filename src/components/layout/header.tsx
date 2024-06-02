5671766defae5d7ba940cfad02adc19c81ac1099
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-06-02 18:21:00 +0200

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import SecondaryButton from "@/components/buttons/SecondaryButton";
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { LogIn, Play } from "lucide-react";
+import { LogIn, LayoutDashboard, Shield, BugPlay } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import PrimaryButton from "../buttons/PrimaryButton";
@@ -9,34 +9,52 @@ import { LogoutButton } from "../buttons/auth/AuthButton";
 
 export default async function Header() {
   const session = await auth();
-  let adminCheck;
+  let isAdmin = false;
 
   if (session) {
-    adminCheck = await prisma.user.findFirst({
+    const checkAdmin = await prisma.user.findFirst({
       where: {
         email: session?.user?.email,
       },
       select: {
         admin: true,
-      }
+      },
     });
+    if (checkAdmin) {
+      isAdmin = checkAdmin.admin;
+    }
   }
 
   return (
-    <header className="p-8 m-auto w-full">
-      <nav className="rounded-lg bg-black bg-opacity-20 backdrop-blur-xl flex items-center justify-between px-8 py-4 relative gap-x-6">
-        <div className="flex items-center justify-center gap-8 md:gap-16">
+    <header className="p-4 m-auto w-full">
+      <nav className="rounded-lg bg-black bg-opacity-20 backdrop-blur-xl flex items-center justify-between px-8 py-4 relative">
         <Link href="/">
           <div className="flex items-center gap-5">
             <Image src="/img/logo.png" alt="logo" width={30} height={30} />
-            <h3 className="hidden md:block text-xl font-semibold text-torea-50">Learn404</h3>
+            <h3 className="hidden md:block text-xl font-semibold text-torea-50">
+              Learn404
+            </h3>
           </div>
         </Link>
-        <div className="flex items-center justify-center gap-6 md:gap-12">
-          <Link href="/" className="text-torea-50 hover:text-torea-200 duration-200">Fonctionnalités</Link>
-          <Link href="/" className="text-torea-50 hover:text-torea-200 duration-200">Prix</Link>
-          <Link href="/" className="text-torea-50 hover:text-torea-200 duration-200">A propos</Link>
-        </div>
+        <div className="hidden lg:flex items-center justify-center gap-6 md:gap-12">
+          <Link
+            href="/"
+            className="text-torea-50 hover:text-torea-200 duration-200"
+          >
+            Fonctionnalités
+          </Link>
+          <Link
+            href="/"
+            className="text-torea-50 hover:text-torea-200 duration-200"
+          >
+            Prix
+          </Link>
+          <Link
+            href="/"
+            className="text-torea-50 hover:text-torea-200 duration-200"
+          >
+            A propos
+          </Link>
         </div>
 
         <div className="flex items-center gap-2">
@@ -44,10 +62,12 @@ export default async function Header() {
             <>
               <PrimaryButton redirectTo="/dashboard" type="button">
                 <span className="hidden md:block font-medium">Dashboard</span>
+                <LayoutDashboard size={20} className="block md:hidden" />
               </PrimaryButton>
-              {adminCheck?.admin && (
+              {isAdmin && (
                 <PrimaryButton redirectTo="/admin" type="button">
                   <span className="hidden md:block font-medium">Admin</span>
+                  <Shield size={20} className="block md:hidden" />
                 </PrimaryButton>
               )}
               <LogoutButton />
@@ -55,12 +75,14 @@ export default async function Header() {
           ) : (
             <>
               <PrimaryButton redirectTo="/waitlist" type="button">
-                <Play size={20} />
+                <BugPlay size={20} />
                 <span className="hidden md:block font-medium">Acheter</span>
               </PrimaryButton>
               <SecondaryButton redirectTo="/login" type="button">
                 <LogIn size={20} />
-                <span className="hidden md:block font-medium">Se connecter</span>
+                <span className="hidden md:block font-medium">
+                  Se connecter
+                </span>
               </SecondaryButton>
             </>
           )}
