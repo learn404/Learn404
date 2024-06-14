1f91926135f29e97313d3ff445e9e73b0493b124
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-06-14 16:25:57 +0200

    :construction_worker: | Edit Price product

--- src/app/api/webhook/create-payment-intent/route.ts
+++ src/app/api/webhook/create-payment-intent/route.ts
@@ -1,19 +1,17 @@
-import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-04-10",
 });
 
-
 export async function POST(req: NextRequest) {
   try {
-    
-    const { email } = await req.json();    
+    const { email } = await req.json();
 
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 599,
+      amount: 50,
       currency: "eur",
       receipt_email: email,
       // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional
@@ -22,12 +20,9 @@ export async function POST(req: NextRequest) {
         enabled: true,
       },
     });
-    
+
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error: any) {
     return NextResponse.json({ error: error.message });
   }
-
-
-
-};
+}
--- src/app/stripe/checkoutForm.tsx
+++ src/app/stripe/checkoutForm.tsx
@@ -1,6 +1,10 @@
-'use client';
+"use client";
 
-import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import {
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
 import { StripePaymentElementChangeEvent } from "@stripe/stripe-js";
 import { FormEvent, useContext, useState } from "react";
 import { EmailContext } from "./PaymentBox";
@@ -15,12 +19,14 @@ type ResponsePayment = {
 export default function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
-  const userEmail = useContext(EmailContext);  
+  const userEmail = useContext(EmailContext);
 
   // const [message, setMessage] = useState<string>("");
   const [isLoading, setLoading] = useState<boolean>(false);
   const [isFormCompleted, setFormCompleted] = useState<boolean>(false);
-  const [responsePayment, setResponsePayment] = useState<ResponsePayment | undefined>(undefined);
+  const [responsePayment, setResponsePayment] = useState<
+    ResponsePayment | undefined
+  >(undefined);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -37,21 +43,24 @@ export default function CheckoutForm() {
         payment_method_data: {
           billing_details: {
             email: userEmail,
-          }
-        }
-      }
-    })
+          },
+        },
+      },
+    });
 
     if (error) {
       setLoading(false);
-      setResponsePayment({ error: error.message, type: "Error"});
+      setResponsePayment({ error: error.message, type: "Error" });
     } else {
       setLoading(false);
       console.log(paymentIntent);
-      setResponsePayment({ success: "Your payment has been successfully processed. You can now access your dashboard.", type: "Success" });
+      setResponsePayment({
+        success:
+          "Votre paiement a été traité avec succès. Vous pouvez maintenant accéder à votre tableau de bord.",
+        type: "Success",
+      });
     }
-
-  }
+  };
 
   const handleStripeChange = (event: StripePaymentElementChangeEvent): void => {
     if (event.complete) {
@@ -61,31 +70,31 @@ export default function CheckoutForm() {
         setFormCompleted(false);
       }
     }
-  }
-  
+  };
+
   return (
-    <> 
-      { responsePayment && (
-        <PopupResponse {...responsePayment} />
-      )}
+    <>
+      {responsePayment && <PopupResponse {...responsePayment} />}
       <form id="payment-form" onSubmit={handleSubmit}>
-        <PaymentElement  onChange={handleStripeChange} />
+        <PaymentElement onChange={handleStripeChange} />
         <div className="mt-4 flex items-center justify-end gap-2 ">
-          <button disabled={isLoading || !isFormCompleted || !!responsePayment} id="submit" className="bg-torea-800 border-2 border-torea-800 px-8 py-3.5 rounded-full 
+          <button
+            disabled={isLoading || !isFormCompleted || !!responsePayment}
+            id="submit"
+            className="bg-torea-800 border-2 border-torea-800 px-8 py-3.5 rounded-full
             font-semibold text-torea-50 enabled:hover:bg-indigo-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-            focus-visible:outline-indigo-500 disabled:cursor-not-allowed disabled:bg-torea-800/50 disabled:border-torea-800/50 disabled:text-gray-500">
+            focus-visible:outline-indigo-500 disabled:cursor-not-allowed disabled:bg-torea-800/50 disabled:border-torea-800/50 disabled:text-gray-500"
+          >
             <span id="button-text">
-              {isLoading ? (
-                "Loading..."
-              ) : responsePayment ? (
-                responsePayment?.type     
-              ) : (
-                "Confirm payment EUR 5.99"     
-              )}
+              {isLoading
+                ? "Chargement..."
+                : responsePayment
+                  ? responsePayment?.type
+                  : "Confirmation du paiement EUR 5.99."}
             </span>
           </button>
         </div>
       </form>
     </>
-  )
+  );
 }
