40f6eb2e7fff1adbd33191d3d2d2938e1ff56175
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-06-19 19:05:55 +0200

    :lipstick: | update landing page

---
+++ src/components/layout/avis.tsx
@@ -0,0 +1,103 @@
+import { cn } from "@/lib/utils";
+import Marquee from "@/components/magicui/marquee";
+
+const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://avatar.vercel.sh/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://avatar.vercel.sh/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/john",
+  },
+  {
+    name: "Jane",
+    username: "@jane",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jane",
+  },
+  {
+    name: "Jenny",
+    username: "@jenny",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jenny",
+  },
+  {
+    name: "James",
+    username: "@james",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/james",
+  },
+];
+
+const firstRow = reviews.slice(0, reviews.length / 2);
+const secondRow = reviews.slice(reviews.length / 2);
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "border-[#2E3038]  hover:bg-gray-50/[.15]",
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-md font-medium text-white">
+            {name}
+          </figcaption>
+          <p className="text-sm font-medium text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-4 text">{body}</blockquote>
+    </figure>
+  );
+};
+
+const MarqueeDemo = () => {
+  return (
+    <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg py-20 md:shadow-xl">
+      <div className=" flex flex-col items-center justify-center  gap-6 text-center max-w-md mb-10">
+        <h2 className=" text-5xl font-semibold titleStyle">Leur avis</h2>
+        <p className="text-lg text-torea-50">
+          Découvrez ce que nos clients pensent de nous.
+        </p>
+      </div>
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {firstRow.map((review) => (
+          <ReviewCard key={review.username} {...review} />
+        ))}
+      </Marquee>
+      <Marquee reverse pauseOnHover className="[--duration:20s]">
+        {secondRow.map((review) => (
+          <ReviewCard key={review.username} {...review} />
+        ))}
+      </Marquee>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-black"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/5 bg-gradient-to-l from-black"></div>
+    </div>
+  );
+};
+
+export default MarqueeDemo;
--- src/components/layout/prices.tsx
+++ src/components/layout/prices.tsx
@@ -4,10 +4,10 @@ import PrimaryButton from "../buttons/PrimaryButton";
 export default function PricesSection() {
   return (
     <div
-      className="w-full flex items-center justify-center py-24 px-4 text-torea-50"
+      className="w-full flex items-center justify-center py-24 px-4 text-torea-50 relative"
       id="prices"
     >
-      <div className="mx-auto p-10 md:p-14 rounded-3xl shadow-pricing-shadow">
+      <div className="mx-auto p-10 md:p-14 rounded-3xl shadow-pricing-shadow z-50">
         <div className="max-w-md flex flex-col gap-6">
           <h2 className=" text-3xl md:text-5xl font-bold ">
             Deviens développeur web
@@ -48,6 +48,18 @@ export default function PricesSection() {
           </div>
         </div>
       </div>
+
+      <img
+        className="absolute left-0 top-0 select-none w-[40rem] h-auto"
+        src="/img/LightsLeft 2.webp"
+        alt="background"
+      />
+      <img
+        className="absolute right-0 top-0 select-none w-[40rem] h-auto"
+        src="/img/LightsRight 2.webp"
+        alt="background"
+      />
+      {/* <div className="pointer-events-none absolute w-full bottom-0 right-0 h-20 bg-gradient-to-t from-[#02030C]"></div> */}
     </div>
   );
 }
