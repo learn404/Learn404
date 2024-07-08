d856d8c7284e4aa41c78df77c21d32a64b87fafb
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-07-08 18:07:04 +0200

    :zap: improve performance code

--- src/components/layout/headerDashboard/headerDashboard.tsx
+++ src/components/layout/headerDashboard/headerDashboard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import UserDropdown from "./userDropDown";
 
+
 interface HeaderDashboardProps {
   user: currentUserType;
   title: string;
