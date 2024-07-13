eb4b98c1f1714ba20a4d5ce3c6ed7e8128e1654a
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-07-13 20:40:06 +0200

    ✨ ADD email wishlist

--- src/app/api/auth/wishlist/route.ts
+++ src/app/api/auth/wishlist/route.ts
@@ -1,29 +1,53 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_SECRET_KEY);
 
 export async function POST(request: Request) {
   try {
-    const { email, } = await request.json();
+    const { email } = await request.json();
 
-    const user = await prisma.wishtlist.findFirst({
-      where: { email },
-      select: {
-        id: true,
+    console.log(email);
+
+    const user = await prisma.wishlist.findFirst({
+      where: {
+        email: email,
       },
     });
 
-    if (!user) {
+    if (user) {
       return NextResponse.json(
-        { message: "Tu es déjà inscris à la WhiteList" },
-        { status: 404 },
+          { message: "Tu es déjà inscris à la wishlist" },
+          { status: 404 },
       );
-    }
+  }
 
-    await prisma.wishtlist.create({
+    await prisma.wishlist.create({
       data: {
         email: email,
       },
     });
+
+
+
+    await resend.emails.send({
+      from: 'Learn404 <noreply@learn404.com>',
+      to: [email],
+      subject: 'Hello world',
+      html: '<p>Hello world</p>',
+    });
+
+
+await resend.contacts.create({
+  email: email,
+  unsubscribed: false,
+  audienceId: '6029319e-c00c-4af2-984c-d5e97e5d3050'
+
+});
+
+
+
 
     return NextResponse.json(
       { message: "Tu es ajouté à la Wishlist" },
