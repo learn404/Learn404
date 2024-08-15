0c5e093b052fffba587100d5bd0bba616246a5c3
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-08-15 13:17:22 +0200

    add keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://learn404.com'),
-  keywords: ['Next.js', 'React', 'JavaScript', 'Développement', 'Web', 'Formation', 'Learn404', 'Formation en Développement Web', 'Formation en Développement Web avec Next.js', 'Formation en Développement Web avec React', 'Formation en Développement Web avec JavaScript', 'Formation en Développement Web avec Git', 'Formation en ligne'],
+  keywords: ['Next.js', 'React', 'JavaScript', 'Développement', 'Web', 'Formation', 'Learn404', 'Formation en Développement Web', 'Formation en Développement Web avec Next.js', 'Formation en Développement Web avec React', 'Formation en Développement Web avec JavaScript', 'Formation en Développement Web avec Git', 'Formation en ligne', "formation en ligne","formation developpeur web", "formation informatique","developpeur web full stack studi","site web","visual studio code","developpeur web","développeur","développeur web salaire","formation developpeur"],
   title: "Learn404 - Formation en Développement Web",
   description:
     "Devenez expert en création et optimisation de sites web avec notre formation complète en Développement Web. Apprenez les bases du HTML, CSS, et JavaScript, maîtrisez des frameworks comme React, Angular et plus encore, et les outils de développement web comme Git,...",
